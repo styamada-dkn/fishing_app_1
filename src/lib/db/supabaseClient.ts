25f2bb69a539d20ece3e467469d6c8dcc512183c
styamada-dkn <167979427+styamada-dkn@users.noreply.github.com>
2024-06-12 15:56:12 +0900

    Update supabaseClient.ts
    Vercelデプロイ対応で元のコードに戻す

--- src/lib/db/supabaseClient.ts
+++ src/lib/db/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { Database } from "@/constants/schema";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_ANON_KEY!;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
   auth: {
